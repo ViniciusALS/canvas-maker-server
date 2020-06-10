c9c06253d4968fc004fbb2129f41f2de5e11dc3f
Vinicius Souza <me@viniciusals.com>
2020-06-10 20:39:30 -0300

    Define user request paths

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3333;
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/create-account', ()=>{});
+app.post('/create-account', ()=>{});
+app.get('/login', ()=>{});
 
 app.listen(port);
