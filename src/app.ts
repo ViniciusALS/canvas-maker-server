faf1707454b35d046c7b09206850b2137ae48b02
Vinicius Souza <me@viniciusals.com>
2020-08-31 13:13:06 -0400

    Makes express use cors to allow cross origin requests

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(helmet());
 
